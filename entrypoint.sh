d1b9fc1a692d23f36fb4f479a9ed61857e3d2543
Muhammadjon <rahmatullayevmuhammadjon042@gmail.com>
2025-05-16 11:32:08 +0500

    asfsf

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 set -e
 
+echo "Ma'lumotlar bazasi tayyorligini kutyapmiz..."
+while ! nc -z db 5432; do
+  echo "Postgres serveriga ulanish imkoni yo'q, 2 soniya kutilyapti..."
+  sleep 2
+done
+
+echo "Ma'lumotlar bazasi tayyor, migratsiyalarni qo'llayapmiz..."
+
 # EF tool o'rnatish (birinchi marta kerak bo'ladi)
 dotnet tool install --global dotnet-ef
 
