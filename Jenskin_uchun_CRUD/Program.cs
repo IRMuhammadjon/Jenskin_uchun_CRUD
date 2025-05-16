3dec3100e3b7e6eea151b0cf792927ef023db055
Muhammadjon <rahmatullayevmuhammadjon042@gmail.com>
2025-05-16 11:35:50 +0500

    fdaf

--- Jenskin_uchun_CRUD/Program.cs
+++ Jenskin_uchun_CRUD/Program.cs
@@ -7,15 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddControllers();
-builder.Services.AddCors(options =>
-{
-    options.AddDefaultPolicy(policy =>
-    {
-        policy.AllowAnyOrigin()
-              .AllowAnyHeader()
-              .AllowAnyMethod();
-    });
-});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -29,7 +21,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-app.UseCors();
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
